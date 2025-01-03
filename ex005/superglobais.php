510b73e7c4322508713fb39cb56f27de49ca391a
lauanderson-rael <lauanderson38@gmail.com>
2025-01-03 00:41:38 -0300

    added d005

---
+++ ex005/superglobais.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superglobais</title>
+</head>
+
+<body>
+    <main>
+        <pre>
+        <?php
+
+        setcookie("dia", "quarta", time() + 3600);
+        session_start();
+        $_SESSION["nome"] = "Matheus";
+
+        echo "<h1 style='color:blue'>Superglobal GET </h1>";
+        var_dump($_GET);
+
+        echo "<h1 style='color:blue'>Superglobal POST </h1>";
+        var_dump($_POST);
+
+        echo "<h1 style='color:blue'>Superglobal REQUEST </h1>";
+        var_dump($_REQUEST);
+
+        echo "<h1>Superglobal COOKIE </h1>";
+        var_dump($_COOKIE);
+
+        echo "<h1>Superglobal SESSION </h1>";
+        var_dump($_SESSION);
+
+        echo "<h1>Superglobal FILES </h1>";
+        var_dump($_FILES);
+
+        echo "<h1>Superglobal ENV </h1>";
+        var_dump($_ENV);
+
+        echo "<h1>Superglobal SERVER </h1>";
+        var_dump($_SERVER);
+
+        echo "<h1 style='color:red'>Superglobal GLOBALS </h1>";
+        var_dump($GLOBALS);
+
+        ?>
+    </pre>
+    </main>
+
+</body>
+
+</html>
