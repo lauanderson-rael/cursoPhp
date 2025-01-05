8375334ac0991ea1cd9a06d19484eb87706e64b3
lauanderson-rael <lauanderson38@gmail.com>
2025-01-05 00:28:19 -0300

    vercel

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 013</title>
-    <link rel="stylesheet" href="styles1.css">
+    <link rel="stylesheet" href="/public/style.css">
 </head>
 
 <body>
